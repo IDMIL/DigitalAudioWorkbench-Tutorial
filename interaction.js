9e115357a2ea12b21c78037beff54423c6f50bad
ArdenButterfield <ArdenButterfield@users.noreply.github.com>
2025-12-08 15:14:18 +0000

    Deploying to gh-pages from @ IDMIL/DigitalAudioWorkbench-Tutorial@8e74c1a99dbf5860b58b6926df494011122b55df 🚀

---
+++ interaction.js
@@ -0,0 +1,20 @@
+function collapseClick(id) {
+  let collapse = document.getElementById(id).parentNode.parentNode  .querySelector('.collapse');
+  if (collapse.style.display === 'none') {
+    collapse.style.display = 'block';
+    document.getElementById(id).textContent = "_";
+  } else {
+    collapse.style.display = 'none';
+    document.getElementById(id).textContent = "\u26F6";
+  }
+}
+
+function collapseGlobalPanel() {
+  let collapse = document.getElementById("global-panel-collapse-button").parentNode.querySelector('.contents');
+  if (collapse.style.display === 'none') {
+    collapse.style.display = 'block';
+  } else {
+    collapse.style.display = 'none';
+  }
+
+}
